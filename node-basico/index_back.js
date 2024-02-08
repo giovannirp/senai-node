dc62283aee92ce20ec4f58f8e3c02fd0f98d2084
Giovanni <giovanni_ge@hotmail.com>
2024-02-08 14:30:37 -0300

    back

--- node-basico/index_back.js
+++ node-basico/index_back.js
@@ -3,11 +3,10 @@ const http = require('http');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Gigi!');
+  res.setHeader('Content-Type','text/plain; charset=utf-8');
+  res.end('OLá Gigi!');
 });
 
 server.listen(port, hostname, () => {
