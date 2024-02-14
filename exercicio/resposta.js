ce45b1230cfb92b2aee7945d67a4d1dd9d92a28f
giovanni <giovanni_ge@hotmai.com>
2024-02-14 11:46:26 -0300

    adicionando novos itens

---
+++ exercicio/resposta.js
@@ -0,0 +1,32 @@
+/*
+  01
+
+  - Através do encadeamento de 2 métodos, gere um novo array com apenas os nomes  
+    dos amigos que moram perto (que contém a propriedade "nearMe" armazenando 
+    true).
+*/
+
+const friends = [
+  { id: 1, name: 'João', nearMe: true },
+  { id: 2, name: 'Matheus', nearMe: true },
+  { id: 3, name: 'Luana', nearMe: false },
+  { id: 4, name: 'Nilson', nearMe: true },
+  { id: 5, name: 'Solange', nearMe: false }
+];
+
+const friendNear = friends
+  .map((item) => item)
+  .filter(({ nearMe }) => nearMe === true)
+
+console.log(friendNear);
+
+
+/*
+  02
+
+  - Encontre e exiba no console o 1º item maior que 50 do array abaixo.
+*/
+
+const randomNumbers = [10, 5, 0, 40, 60, 10, 20, 70];
+
+const resultItem = randomNumbers.find((item) => item > 50);
