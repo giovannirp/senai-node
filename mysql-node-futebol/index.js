ed728c52a6605aed468a1db36ad552bbeb0e58ee
giovanni <giovanni_ge@hotmai.com>
2024-03-19 21:23:32 -0300

    feat: adicionando projeto

---
+++ mysql-node-futebol/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('home')
+});
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'futebol'
+});
+
+conn.connect(function(err) {
+  if (err) {
+    console.log(err);
+  }
+
+  console.log('Conectou ao MSYQL');
+
+  app.listen(3000);
+})
+
