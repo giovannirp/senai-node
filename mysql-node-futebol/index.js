fee2c248e49b3bb55d07993472418429a331b678
Giovanni <giovanni_ge@hotmail.com>
2024-03-23 18:00:12 -0300

    feat: adicionando

--- mysql-node-futebol/index.js
+++ mysql-node-futebol/index.js
@@ -1,79 +1,89 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const mysql = require('mysql');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
 const app = express();
 
 // Configura o middleware para analisar solicitações com o tipo de conteúdo body
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
 
 // Configura o middleware para analisar solicitações com o tipo de conteúdo 'application/json'
-app.use(express.json())
+app.use(express.json());
 
-app.engine('handlebars', exphbs.engine())
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine());
+app.set("view engine", "handlebars");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.render('home')
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-// inserindo dados
-app.post('/listagem', (req, res) => {
-  const sql =  "SELECT * FROM times";
+app.get("/listas", (req, res) => {
+  const sql = "SELECT * FROM times";
 
-  conn.query(sql, function(err, data) {
+  conn.query(sql, function (err, data) {
     if (err) {
       console.log(err);
       return;
     }
 
     const fut = data;
-     
-    res.render('listagem', { fut })
-  })
-})
+    console.log("dados do banco", fut);
+    res.render("listas", { fut });
+  });
+});
 
-// cadastrando  
-app.post('/listagem/insertbook', (req, res) => {
+// inserindo dados
+// app.post('/listas', (req, res) => {
+//   const sql =  "SELECT * FROM times";
+
+//   conn.query(sql, function(err, data) {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+
+//     const fut = data;
+//     console.log("dados do banco", fut)
+//     res.render('listas', { fut })
+//   })
+// })
+
+// cadastrando
+app.post("/listagem/insertbook", (req, res) => {
   const nome = req.body.nome;
   const titulo = req.body.titulo;
 
   const sql = `INSERT INTO times (nome, titulo) values ('${nome}', '${titulo}')`;
 
-  conn.query(sql, function(err) {
+  conn.query(sql, function (err) {
     if (err) {
       console.log("novo err", err);
       return;
     }
 
-    res.redirect('/');
-  })
-
-
-
-
-})
-
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'futebol'
+    res.redirect("/");
+  });
 });
 
-conn.connect(function(err) {
+const conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "futebol",
+});
+
+conn.connect(function (err) {
   if (err) {
     console.log(err);
   }
 
-  console.log('Conectou ao MSYQL');
+  console.log("Conectou ao MSYQL");
 
   app.listen(3000);
-})
-
+});
