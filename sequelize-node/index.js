acb7122514d8208c5b27b5610cc14443d950260b
giovanni <giovanni_ge@hotmai.com>
2024-04-20 12:37:09 -0300

    feat: adicionando cadastro de formulario

--- sequelize-node/index.js
+++ sequelize-node/index.js
@@ -18,7 +18,29 @@ app.use(
 
 app.use(express.json())
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+app.get('/users/create', (req, res) => {
+  res.render('adduser');
+});
+
+app.post('/users/create', async (req, res) => {
+  const name = req.body.name;
+  const occupation = req.body.occupation;
+  let newslleter = req.body.newslleter;
+
+  if (newslleter === 'on') {
+    newslleter = true;
+  } else {
+    newslleter = false;
+  }
+
+  console.log(req.body)
+
+  await User.create({ name, occupation, newslleter });
+
+  res.redirect('/');
+});
 
 app.get('/', function (req, res) {
   res.render('home')
